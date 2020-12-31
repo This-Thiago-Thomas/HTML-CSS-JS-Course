84de7937c2f9731fa51679ecdd8e2a146b7f4570
Thiago <thiago_rocha17@hotmail.com>
2020-12-31 12:27:02 -0300

    55 - Basic math in JavaScript — numbers and operators

---
+++ 03 - JS/55 - Basic math in JavaScript — numbers and operators/55.1 - Number Types in JS.js
@@ -0,0 +1,6 @@
+let myInt = 5;
+let myFloat = 87.08;
+console.log(myInt);
+console.log(myFloat);
+console.log(typeof myInt);  //Number (integer but in js is Number)
+console.log(typeof myFloat); //Number (float but in js is Number)
