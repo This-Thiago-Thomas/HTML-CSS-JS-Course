9addb8c18f43d626237bd392342abf687cfafeba
Thiago <thiago_rocha17@hotmail.com>
2021-01-09 15:53:15 -0300

    60 - Making decisions in your code — conditionals

---
+++ 03 - JS/60 - Making decisions in your code — conditionals/60.3 - Ternary operator.js
@@ -0,0 +1,3 @@
+let isYourBirthday = true;
+let message = (isYourBirthday) ? "Happy Birthday" : "Good Morning";
+console.log(message);
---
+++ 03 - JS/60 - Making decisions in your code — conditionals/60.5 - Password Exercise.js
@@ -0,0 +1,9 @@
+let machineActive = true;
+let pwd = 'cheese';
+
+// Add your code here
+if(!machineActive) {
+  console.log('Please turn on the machine!');
+} else {
+  (pwd == 'cheese') ? console.log('Password Correct') : console.log('Incorrect Password');
+}
